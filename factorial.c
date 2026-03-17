5433831b673a04d825b39eb2300d48fc4f5134d9
ASHISH <kanhaiyakj444@gmail.com>
2026-03-17 16:00:15 +0530

    FACTORIAL
    CLASS WORK QUES

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int n,sum=1;
+    printf("enter a number :\n");
+    scanf("%d", &n);
+
+
+    for(int i = 1 ; i <= n ; i++) {
+        sum *= i;
+
+    }
+    printf("sum of first natural number =%d \n",sum);
+    return 0;
+}
